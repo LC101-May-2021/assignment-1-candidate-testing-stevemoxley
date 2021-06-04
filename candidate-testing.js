57cf5a3e31e0527c5d318d13f1653ade5b5cc024
Steve Moxley <>
2021-06-04 19:57:50 +0000

    test

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,45 +3,85 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+let candidateName = "";
+
+// TODO 1.2a: Define question, correctAnswer, and candidateAnswer 
 let question;
 let correctAnswer;
 let candidateAnswer;
 let questions;
 let correctAnswers;
 let candidateAnswers;
-
+let numberOfCorrAns =0;
+let percScored =0.0;
+let grade;
+//question = "Who was the first American woman in space? ";
+//correctAnswer = "Sally Ride";
+questions = ['1) Who was the first American woman in space?', '2)True or false: 5 kilometer == 5000 meters?','3) (5 + 3)/2 * 10 = ?','4) Given the array[8,Orbit,Trajectory,45],what entry is at index 2?', '5) What is the minimum crew size for the ISS?'];
+correctAnswers = ['Sally Ride','true','40','Trajectory','3'];
+//candidateAnswers="";
+candidateAnswers = [5];         
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+  candidateName = input.question("Please enter the candidate name: ");
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+//candidateAnswer = input.question(question);
+  for (i = 0; i < questions.length; i++) {
+    candidateAnswers[i] = input.question(questions[i]);
+    let yourAns = `Your Answer:  ${candidateAnswers[i]}`;
+    let corrAns = `Correct Answer: ${correctAnswers[i]}`;
+  //console.log("Your Answer: "+candidateAnswers[i]);
+  //console.log("Correct Answer: "+correctAnswers[i]);
+    console.log(yourAns);
+    console.log(corrAns);
+      if(correctAnswers[i].toUpperCase() === candidateAnswers[i].toUpperCase()){
+        numberOfCorrAns++;
+  }
+ }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
-  let grade;
   
-
-  return grade;
+   percScored = (numberOfCorrAns/questions.length)*100;
+   //console.log(numberOfCorrAns);
+   //console.log(questions.length);
+    
+        if (percScored >=80){
+            grade ="Passed";
+       }else {
+            grade ="Failed";
+      }
+        console.log(">>> Overall Grade: " + percScored +"% (" + numberOfCorrAns + " of " +  questions.length + " responses correct)" + "<<<");
+        console.log(">>> Status: " + grade + " <<<");
+        return percScored;
 }
+
+  
+    
+/*if (candidateAnswer === correctAnswer){
+   console.log("correct Answer");
+} else{
+
+  console.log("Incorrect Answer");
+}
+ 
+  return grade;*/
+
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log("Candidate Name: "+ candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
-}
+} 
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
